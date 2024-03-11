e96d753e45d3a916f316b9d765f11463bbfd9e23
eggzuxi <eggzuxi99@gmail.com>
2024-03-11 11:28:52 +0900

    PJH_forpull

---
+++ config/dbConnect.js
@@ -0,0 +1,18 @@
+// const { MongoClient } = require('mongodb');
+// const uri = process.env.DB_ATLAS_URL;
+
+// const client = new MongoClient(uri);
+// const dbName = 'test';
+
+// async function main() {
+//    await client.connect();
+//    console.log('Connected successfully to server');
+//    const db = client.db(dbName);
+//    const collection = db.collection('user');
+//    return 'done.';
+// }
+
+// main()
+// .then(console.log)
+// .catch(console.error)
+// .finally(() => client.close());
