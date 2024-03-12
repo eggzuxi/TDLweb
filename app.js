1b2841e048b31ed8ccdcb6b295ef0cb8e3188637
eggzuxi <eggzuxi99@gmail.com>
2024-03-12 11:41:02 +0900

    pjh_mainpage연결

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use("/login",require("./routes/loginRoutes"));
 app.use("/register",require("./routes/registerRoutes"));
 
 // app.use("/",require("./routes/loginRoutes"));
-app.use("/contacts", require("./routes/contactRoutes"));
+app.use("/", require("./routes/contactRoutes"));
 
 app.listen(PORT, () => {
   console.log(`Server listening from http://localhost:${PORT}`);
