96c757a9edcf68e7091d17020fd801f4d642603d
eggzuxi <eggzuxi99@gmail.com>
2024-03-13 08:41:15 +0900

    pjh_오류수정

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {getLogin,loginUser} = require("../controllers/loginController")
 // logout 추가?
 
-router.route("/login").get(getLogin).post(loginUser)
+router.route("/").get(getLogin).post(loginUser)
 // router.route("/logout").get(logout);
 
 module.exports = router;
