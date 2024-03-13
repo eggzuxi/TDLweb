d49e437f15014937dd2c99d014d4c17b93386731
Park HyeonJun <baghyeonjun16@gmail.com>
2024-03-13 11:22:12 +0900

    PHJ_todo list출력 및 추가 작업

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,20 +2,25 @@ const asyncHandler = require("express-async-handler");
 const Todo = require("../models/todoModel");
 const bcrypt = require("bcrypt");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JW_SECRET
 
 
-const getAllTodo = asyncHandler(async(req,res)=>{
-  const contacts = await Todo.find();
-  res.render("todo",{todo:contacts})
+const getAllTodo = asyncHandler(async (req, res) => {
+  const Email = req.cookies.Email;
+  const userTodos = await Todo.findMany({ Email: Email});
+  res.render("todo", { todo: userTodos });
 });
 
+
 const createTodo = asyncHandler(async(req,res)=>{
+  const Email = req.cookies.Email;
   const {title,body} = req.body;
   console.log(req.body)
   if(!title||!body){
     return res.status(400).send("필수값입력안함")
   }
-  const contact = await Todo.create({title,body});
+  const contact = await Todo.create({Email,title,body});
   res.status(201).send("리스트 추가");
 });
 
@@ -30,9 +35,9 @@ const updateTodo = asyncHandler(async(req,res)=>{
 });
 
 const deleteTodo = asyncHandler(async(req,res)=>{
-  const id = req.params.id;
-  const contact = await Todo.findByIdAndDelete(id);
-  res.status(200).send(`Delete Contact for Id:${req.params.id}`);
+  const title = req.params.title;
+  const contact = await Todo.findByIdAndDelete(title);
+  res.status(200).send(`Delete Contact for Id:${req.params.title}`);
 });
 
 module.exports = {getAllTodo,createTodo,updateTodo,deleteTodo};
--- models/todoModel.js
+++ models/todoModel.js
@@ -2,13 +2,16 @@ const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema(
   {
+    email:{
+      type:String,
+      require:true
+    },
     title :{
       type:String,
       require:true,
     },
     body :{
       type:String,
-      require:true,
     },
   }
 );
